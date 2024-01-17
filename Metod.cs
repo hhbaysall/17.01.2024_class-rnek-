e1d4af068fd467c892fb00ba83ac5222f9f44a3f
hhbay <hhbay@HALILIBO>
2024-01-17 19:55:43 +0300

    Add project files.

---
+++ Metod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._01._2024_classörnek_
+{
+    internal class Metod
+    {
+
+        public int Toplam(int[] sayiDizisi)
+        {
+            int toplam  = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            return toplam;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._01._2024_classörnek_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // kullanıcıdan kaç adet sayı girmek istediğini sorup top ve ort yazdıralım.
+
+            // sayı adedi ve isteme işini Main metodu içerisinde yapabiliriz.
+            //Daha sonra oluşturulan diziyi toplam işlemi yapıp sonucu döndürebiliriz.
+
+            Console.Write("Adet giriniz: ");
+            int adet=int.Parse(Console.ReadLine());
+            int[]sayilar=new int[adet];
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write("{0}. sayıyı giriniz: ",i+1);
+                sayilar[i]= int.Parse(Console.ReadLine());
+            }
+            Metod m=new Metod();
+            m.Toplam(sayilar);
+
+            Console.WriteLine("Toplam: " + m.Toplam(sayilar));
+            Console.ReadKey();
+        }
+    }
+}
